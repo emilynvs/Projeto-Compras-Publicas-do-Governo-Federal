731d7e0c545ed91072db27d1284285c8c7eb9cc7
pedrovitor004 <pedrovitorbarbosaflorentino@gmail.com>
2025-11-21 00:48:04 -0300

    estruturando o projeto e adicionando novos modulos para os proximos passos

---
+++ modulo_api/main.py
@@ -0,0 +1,21 @@
+
+# Nossa aplicação tem o objetivo de utilizar uma base de dados semisestruturada do dados.gov
+# A base escolhida foi a de Salas da Ancine por todo o país 
+# Diante disso escolhemos a temática de analisar os dados e descobrimos que daria para fazer uma análise de dados com relação a 
+# Acessibilidade das salas
+# Usando: ASSENTOS_CADEIRANTES, ASSENTOS_MOBILIDADE_REDUZIDA, ASSENTOS_OBESIDADE, ACESSO_ASSENTOS_COM_RAMPA, ACESSO_SALA_COM_RAMPA, BANHEIROS_ACESSIVEIS
+# Como  Análises possíveis: 
+# Quais estados possuem salas mais acessíveis? 
+# Percentual de salas com acessibilidade completa?
+# Quais cidades têm melhores condições para pessoas com necessidades especiais?
+
+def selecaoDecolunas():
+    return
+def analiseEstados():
+    return
+def percentualSalaComacessebilidade():
+    return
+def melhoresCidades():
+    return
+def menu():
+    return 
